6fa67ed053e6b5271a9f4a1be757fd5f45e71292
Chien-Chi Wang <oiu850714@gmail.com>
2023-02-05 22:11:11 +0800

    load and store plugin states

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -188,6 +188,8 @@ void ThreeBandCompressorOiuAudioProcessor::getStateInformation(
   // You should use this method to store your parameters in the memory block.
   // You could do that either as raw data, or use the XML or ValueTree classes
   // as intermediaries to make it easy to save and load complex data.
+  juce::MemoryOutputStream mos(destData, true);
+  apvts.state.writeToStream(mos);
 }
 
 void ThreeBandCompressorOiuAudioProcessor::setStateInformation(
@@ -195,6 +197,10 @@ void ThreeBandCompressorOiuAudioProcessor::setStateInformation(
   // You should use this method to restore your parameters from this memory
   // block, whose contents will have been created by the getStateInformation()
   // call.
+  auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+  if (tree.isValid()) {
+    apvts.replaceState(tree);
+  }
 }
 
 //==============================================================================
