ebe4869b3fa1256dd6493112e9be713396f299fb
Chien-Chi Wang <oiu850714@gmail.com>
2023-03-18 23:35:03 +0800

    remove unused hello world sample code

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -42,17 +42,7 @@ ThreeBandCompressorOiuAudioProcessorEditor::
 }
 
 //==============================================================================
-void ThreeBandCompressorOiuAudioProcessorEditor::paint(juce::Graphics& g) {
-  // (Our component is opaque, so we must completely fill the background with a
-  // solid colour)
-  g.fillAll(
-      getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
-
-  g.setColour(juce::Colours::white);
-  g.setFont(15.0f);
-  g.drawFittedText("Hello World!", getLocalBounds(),
-                   juce::Justification::centred, 1);
-}
+void ThreeBandCompressorOiuAudioProcessorEditor::paint(juce::Graphics&) {}
 
 void ThreeBandCompressorOiuAudioProcessorEditor::resized() {
   auto bounds = getLocalBounds();
